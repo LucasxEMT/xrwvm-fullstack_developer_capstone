b9b6d8d2db2d3311f0ec5e74850c20a2e62889fd
LucasxEMT <lucas@balancedcode.com>
2025-02-26 05:15:57 -0500

    Added register view and route for Django + updated React routes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,20 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // <-- Import Register component
 
 function App() {
   return (
     <Routes>
+      {/* Route for the Login page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Route for the Register page */}
+      <Route path="/register" element={<Register />} />
+
+      {/* You may also add a default/Home route if needed */}
+      {/* <Route path="/" element={<Home />} /> */}
     </Routes>
   );
 }
+
 export default App;
