0b4954494adfbab8cdb6b099ce337fd122c0b427
LucasxEMT <Lucas@balancedcode.com>
2025-04-08 19:35:28 +0200

    Update dealership.js

--- server/database/dealership.js
+++ server/database/dealership.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+/* jshint esversion: 10 */
+'use strict';
 
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dealerships = new Schema({
-	id: {
+  id: {
     type: Number,
-    required: true,
-	},
-	city: {
+    required: true
+  },
+  city: {
     type: String,
     required: true
   },
@@ -32,7 +34,7 @@ const dealerships = new Schema({
     required: true
   },
   short_name: {
-    type: String,
+    type: String
   },
   full_name: {
     type: String,
